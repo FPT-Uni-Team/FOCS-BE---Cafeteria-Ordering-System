e2e7c719d59b54d5818b85527600d282ee40ea78
phucnh <phucnhhe172629@fpt.edu.vn>
2025-05-14 15:10:06 +0700

    feat: Implement identity framework

---
+++ SEP490-TipTrip/TipTrip.Infrastructure.Identity/Persistance/DBContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TipTrip.Infrastructure.Identity.Identity.Model;
+
+namespace TipTrip.Infrastructure.Identity.Persistance
+{
+    internal class DBContext : IdentityDbContext<User>
+    {
+        public DBContext() { }
+        public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+    }
+}
