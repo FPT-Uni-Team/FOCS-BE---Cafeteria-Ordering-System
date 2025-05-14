05d772c294fa330d5b8bf991af205bbc3f3124f5
phucnh <phucnhhe172629@fpt.edu.vn>
2025-05-14 16:52:40 +0700

    feat/implement-identity-framework add base attribute

--- SEP490-TipTrip/TipTrip.Infrastructure.Identity/Persistance/DBContext.cs
+++ SEP490-TipTrip/TipTrip.Infrastructure.Identity/Persistance/DBContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using TipTrip.Common.Attributes;
 using TipTrip.Infrastructure.Identity.Identity.Model;
 
 namespace TipTrip.Infrastructure.Identity.Persistance
@@ -33,6 +34,17 @@ namespace TipTrip.Infrastructure.Identity.Persistance
             modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
             modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
             modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (!typeof(BaseAttributes).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.Name).Property<DateTime>("CreatedAt").HasColumnType("datetime2");
+                    modelBuilder.Entity(entityType.Name).Property<string>("CreatedBy").HasColumnType("nvarchar(max)");
+                    modelBuilder.Entity(entityType.Name).Property<DateTime>("UpdatedAt").HasColumnType("datetime2");
+                    modelBuilder.Entity(entityType.Name).Property<string>("UpdatedBy").HasColumnType("nvarchar(max)");
+                }
+            }
         }
     }
 }
