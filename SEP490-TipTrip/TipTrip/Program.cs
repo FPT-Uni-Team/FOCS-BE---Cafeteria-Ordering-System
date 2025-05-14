cb9ab01816dd23df3211b798647c864d93d988cd
XuanSonHoang <hson512475@gmail.com>
2025-05-14 14:18:45 +0700

    feat: Setup swagger for api documentation (DP-26)

--- SEP490-TipTrip/TipTrip/Program.cs
+++ SEP490-TipTrip/TipTrip/Program.cs
@@ -1,23 +1,75 @@
+﻿using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    // config API
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My Own API",
+        Version = "v1",
+        Description = "API for managing authentication, users, and data operations.",
+        Contact = new OpenApiContact
+        {
+            Name = "SEP490: Capstone project",
+            Email = "hson512475@gmail.com",
+            Url = new Uri("https://yourwebsite.com")
+        }
+    });
+
+    // JWT Bearer
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token format: **Bearer {token}**\n\nExample: `Bearer eyJhbGciOiJIUzI1...`"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Enable Swagger middleware (env Development và Production)
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Own API v1");
+        c.DocumentTitle = "My API Documentation";
+        c.RoutePrefix = "swagger"; // route: /swagger
+        c.DefaultModelsExpandDepth(-1); // Hide Models default if dont need
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
