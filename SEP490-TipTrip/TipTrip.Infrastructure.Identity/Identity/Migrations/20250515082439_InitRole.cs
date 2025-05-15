abb890fbdc8767661c071e6f7cb0aabfa8d9b6c5
phucnh <phucnhhe172629@fpt.edu.vn>
2025-05-15 15:33:03 +0700

    feat: define user roles

---
+++ SEP490-TipTrip/TipTrip.Infrastructure.Identity/Identity/Migrations/20250515082439_InitRole.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TipTrip.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Roles");
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6a25d16f-9e41-431a-86e3-5e7e1620746b", "6a25d16f-9e41-431a-86e3-5e7e1620746b", "Admin", "ADMIN" },
+                    { "cf0a1168-7764-4a16-8e7e-f69c834ba3fe", "cf0a1168-7764-4a16-8e7e-f69c834ba3fe", "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "6a25d16f-9e41-431a-86e3-5e7e1620746b");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "cf0a1168-7764-4a16-8e7e-f69c834ba3fe");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Roles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Roles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
