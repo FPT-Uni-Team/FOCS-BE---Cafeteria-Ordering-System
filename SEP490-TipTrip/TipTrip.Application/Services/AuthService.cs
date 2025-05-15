56e8735300b94fbfd341d6d60bf2ae89869730d7
XuanSonHoang <hson512475@gmail.com>
2025-05-15 16:39:53 +0700

    feat: init folder auth

---
+++ SEP490-TipTrip/TipTrip.Application/Services/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipTrip.Common.Interfaces;
+
+namespace TipTrip.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+    }
+}
