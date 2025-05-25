0c1b2197fe45d80638911126c5202edd929e231f
phucnhhe172629 <155723231+phucnhhe172629@users.noreply.github.com>
2025-05-25 23:25:26 +0700

    feat: change user password (#26)
    * feat: change user password
    
    * fix: using check condition middleware

---
+++ SEP490-FOCS/FOCS.Common/Models/ChangePasswordResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FOCS.Common.Models
+{
+    public class ChangePasswordResponse
+    {
+        [JsonPropertyName("is_succes")]
+        public bool IsSuccess { get; set; }
+
+        [JsonPropertyName("errors")]
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
