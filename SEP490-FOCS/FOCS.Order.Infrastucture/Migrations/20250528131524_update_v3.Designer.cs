169f0e174b4517623013443cb6abedb5f16f0fbb
XuanSonHoang <hson512475@gmail.com>
2025-05-28 20:16:09 +0700

    feat: update db

--- SEP490-FOCS/FOCS.Order.Infrastucture/Migrations/20250528131524_update_v3.Designer.cs
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Migrations/20250528131524_update_v3.Designer.cs
@@ -4,6 +4,7 @@ using FOCS.Order.Infrastucture.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FOCS.Order.Infrastucture.Migrations
 {
     [DbContext(typeof(OrderDbContext))]
-    partial class OrderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250528131524_update_v3")]
+    partial class update_v3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Migrations/20250528131524_update_v3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FOCS.Order.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Feedbacks",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Feedbacks",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Feedbacks");
+        }
+    }
+}
