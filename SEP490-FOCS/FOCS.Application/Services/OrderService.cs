ab89ca9e3f9b524f25aca97e6cee1493a2db8261
XuanSonHoang <hson512475@gmail.com>
2025-05-29 10:21:33 +0700

    feat: add background service for realtime update order wrap

---
+++ SEP490-FOCS/FOCS.Application/Services/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using FOCS.Common.Interfaces;
+using FOCS.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOCS.Application.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<List<OrderDTO>> GetPendingOrdersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SEP490-FOCS/FOCS.Common/Models/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOCS.Common.Models
+{
+    public class OrderDTO
+    {
+        public Guid StoreId { get; set; }
+    }
+}
