b05b1272c025a03e0e0292bf4b447e67a0c4fe19
phucnh <phucnhhe172629@fpt.edu.vn>
2025-05-30 15:34:12 +0700

    feat: get menu by store

---
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Common/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using FOCS.Order.Infrastucture.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FOCS.Order.Infrastucture.Common.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly OrderDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public Repository(OrderDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Interfaces/IMenuService.cs
@@ -0,0 +1,9 @@
+﻿using FOCS.Order.Infrastucture.Entities;
+
+namespace FOCS.Order.Infrastucture.Interfaces
+{
+    public interface IMenuService
+    {
+        Task<List<MenuItemVariant>> GetMenuByStore(Guid storeId);
+    }
+}
