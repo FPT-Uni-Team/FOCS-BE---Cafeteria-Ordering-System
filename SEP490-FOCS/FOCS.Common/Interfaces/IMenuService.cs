bccedec8557153aad8ec65f17643c96a68fbba2e
phucnhhe172629 <155723231+phucnhhe172629@users.noreply.github.com>
2025-06-01 14:44:27 +0700

    feat: get menu item detail (#37)

--- SEP490-FOCS/FOCS.Common/Interfaces/IMenuService.cs
+++ SEP490-FOCS/FOCS.Common/Interfaces/IMenuService.cs
@@ -5,5 +5,6 @@ namespace FOCS.Order.Infrastucture.Interfaces
     public interface IMenuService
     {
         Task<PagedResult<MenuItemDTO>> GetMenuItemByStore(UrlQueryParameters urlQueryParameters, Guid storeId);
+        Task<MenuItemDTO> GetItemVariant(Guid itemId);
     }
 }
