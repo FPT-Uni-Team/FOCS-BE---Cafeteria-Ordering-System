ca10acc5f84f0a9343a584f942e71522850b2f43
XuanSonHoang <hson512475@gmail.com>
2025-06-03 18:31:41 +0700

    feat: implement code base for redis cache data

---
+++ SEP490-FOCS/FOCS.Common/Interfaces/IRedisCacheService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOCS.Common.Interfaces
+{
+    public interface IRedisCacheService
+    {
+        Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
+        Task<T?> GetAsync<T>(string key);
+        Task<bool> RemoveAsync(string key);
+        Task<bool> ExistsAsync(string key);
+    }
+}
