7d6b76145e381b5c75e5e63ad36925b7b10ba59f
XuanSonHoang <hson512475@gmail.com>
2025-06-05 10:22:37 +0700

    hot fix

---
+++ SEP490-FOCS/FOCS/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Formats.Asn1;
+using FOCS.Common.Interfaces;
+using FOCS.Common.Models;
+using System.Security.Claims;
+
+namespace FOCS.Controllers
+{
+    [Route("api/me")]
+    [ApiController]
+    public class AuthController : FocsController
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<AuthResult> LoginAsync(LoginRequest loginRequest)
+        {
+            return await _authService.LoginAsync(loginRequest);
+        }
+
+        [HttpPost("register")]
+        public async Task<bool> RegisterAsync(RegisterRequest registerRequest)
+        {
+            return await _authService.RegisterAsync(registerRequest);
+        }
+
+        [HttpPost("logout")]
+        public async Task Logout()
+        {
+            await _authService.LogoutAsync(UserId);
+        }
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.ForgotPasswordAsync(request.Email);
+
+            if (!result)
+                return NotFound(new { message = "User not found with the provided email." });
+
+            return Ok(new { message = "A new reset password link has been sent to your email." });
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request)
+        {
+            var result = await _authService.ResetPasswordAsync(request);
+            if (!result)
+                return BadRequest("Reset password failed. The token might be invalid or expired.");
+
+            return Ok("Password has been reset successfully.");
+        }
+
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(string email, string token)
+        {
+            var result = await _authService.ConfirmEmailAsync(email, token);
+            if (result)
+            {
+                return Ok(new { message = "Email confirmed successfully!" });
+            }
+            else
+            {
+                return BadRequest(new { message = "Email confirmation failed." });
+            }
+        }
+
+        [HttpPost("change-password")]
+        public async Task<bool> ChangePassword(ChangePasswordRequest request, string email)
+        {
+            return await _authService.ChangePassword(request, Email);
+        }
+    }
+}
---
+++ SEP490-FOCS/FOCS/Program.cs
@@ -0,0 +1,226 @@
+﻿using FOCS.Application.Mappings;
+using FOCS.Application.Services;
+using FOCS.Application.Services.Interface;
+using FOCS.Common.Helpers;
+using FOCS.Common.Interfaces;
+using FOCS.Common.Models;
+using FOCS.Infrastructure.Identity.Common.Repositories;
+using FOCS.Infrastructure.Identity.Common.UnitOfWorks;
+using FOCS.Infrastructure.Identity.Identity.Model;
+using FOCS.Infrastructure.Identity.Persistance;
+using FOCS.Middlewares;
+using FOCS.Order.Infrastucture.Context;
+using FOCS.Order.Infrastucture.Entities;
+using FOCS.Order.Infrastucture.Interfaces;
+using FOCS.Realtime.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Config Serilog for logging
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+builder.Host.UseSerilog();
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.Configure<EmailModels>(builder.Configuration.GetSection("EmailSettings")); // Bind EmailSettings from appsettings.json to EmailModels class
+builder.Services.Configure<OrderBatchingOptions>(builder.Configuration.GetSection("OrderBatchingOptions"));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDBContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IEmailHelper, EmailHelper>()
+                .AddScoped<ITokenService, TokenService>()
+                .AddScoped<IAuthService, AuthService>()
+                .AddScoped<IRepository<UserRefreshToken>, Repository<UserRefreshToken, ApplicationDBContext>>()
+                .AddScoped<IEmailService, EmailService>()
+                .AddScoped<IOrderService, OrderService>()
+                .AddScoped<IKitchenService, KitchenService>()
+                .AddScoped<IMenuService, MenuService>()
+                .AddScoped<IMenuManagementService, MenuManagementService>()
+                .AddScoped<IUnitOfWork, UnitOfWork<ApplicationDBContext>>()
+                .AddScoped<IRepository<Order>, Repository<Order, OrderDbContext>>()
+                .AddScoped<IRepository<MenuItem>, Repository<MenuItem, OrderDbContext>>()
+                .AddScoped<IRepository<Store>, Repository<Store, OrderDbContext>>()
+                .AddScoped<IRepository<MenuItemVariant>, Repository<MenuItemVariant, OrderDbContext>>()
+                .AddSingleton<IRedisCacheService>(sp => new RedisCacheService("localhost:6379"));
+;
+//builder.Services.AddHostedService<OrderBatchingService>();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.MigrationsAssembly("FOCS.Infrastructure.Identity")
+    ));
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.MigrationsAssembly("FOCS.Order.Infrastucture")
+    ));
+
+//auto mapper
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    // config API
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My Own API",
+        Version = "v1",
+        Description = "API for managing authentication, users, and data operations.",
+        Contact = new OpenApiContact
+        {
+            Name = "SEP490: Capstone project",
+            Email = "hson512475@gmail.com",
+            Url = new Uri("https://yourwebsite.com")
+        }
+    });
+
+    // JWT Bearer
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter JWT token format: **Bearer {token}**\n\nExample: `Bearer eyJhbGciOiJIUzI1...`"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+
+    // Lockout settings.
+    options.Lockout.MaxFailedAccessAttempts = 4;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+});
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500") 
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials(); 
+    });
+});
+
+builder.Services.AddSignalR();
+
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+// Add Serilog request logging middleware
+app.UseSerilogRequestLogging();
+//Regis middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+// CORS
+app.UseCors("AllowFrontend");
+
+//SignalR
+app.MapHub<OrderHub>("/orderHubs");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger(); // Swagger on Production
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        c.RoutePrefix = "swagger";
+    });
+
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    Log.Information("Starting web application");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
