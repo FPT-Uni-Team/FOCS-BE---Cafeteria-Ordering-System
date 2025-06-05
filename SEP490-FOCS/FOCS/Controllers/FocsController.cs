3d156af5ae8c017fc7d30826e3809a227840ae57
XuanSonHoang <hson512475@gmail.com>
2025-06-05 10:50:46 +0700

    hot fix recode main

---
+++ SEP490-FOCS/FOCS/Controllers/FocsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FOCS.Controllers
+{
+    [ApiController]
+    public class FocsController : ControllerBase
+    {
+        protected string UserId => User.FindFirstValue(ClaimTypes.NameIdentifier);
+        protected string Email => User.FindFirstValue(ClaimTypes.Email);
+        protected string Role => User.FindFirstValue(ClaimTypes.Role);
+
+        protected string StoreId => User.FindFirstValue("StoreId");
+        protected string AccessToken
+        {
+            get
+            {
+                var token = HttpContext.GetTokenAsync(JwtBearerDefaults.AuthenticationScheme, "access_token").GetAwaiter().GetResult();
+                return token ?? throw new InvalidOperationException("Access token not available.");
+            }
+        }
+
+        protected bool IsAuthenticated => User?.Identity?.IsAuthenticated ?? false;
+    }
+}
