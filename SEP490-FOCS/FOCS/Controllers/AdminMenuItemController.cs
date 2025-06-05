a61f48a06045a16efcbc846d8748237c5c6c268e
Dioak <dioak2002@gmail.com>
2025-06-05 22:54:07 +0700

    feat/update

---
+++ SEP490-FOCS/FOCS/Controllers/AdminMenuItemController.cs
@@ -0,0 +1,75 @@
+﻿using FOCS.Application.DTOs.AdminServiceDTO;
+using FOCS.Application.Services.Interface;
+using FOCS.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FOCS.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminMenuItemController : FocsController
+    {
+        private readonly IMenuManagementService _adminMenuItemService;
+        private readonly IBrandManagementService _adminBrandService;
+        private readonly IStoreManagementService _adminStoreService;
+
+        public AdminMenuItemController(IMenuManagementService menuService, IBrandManagementService adminBrand, IStoreManagementService storeService)
+        {
+            _adminMenuItemService = menuService;
+            _adminBrandService = adminBrand;
+            _adminStoreService = storeService;
+        }
+
+        [HttpPost("menu-item")]
+        public async Task<IActionResult> CreateMenuItem([FromBody] MenuItemAdminServiceDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _adminMenuItemService.CreateMenuAsync(dto, UserId);
+
+            return Ok(created);
+        }
+
+        [HttpPost("menu-items")]
+        public async Task<IActionResult> GetAllMenuItems([FromBody] UrlQueryParameters urlQueryParameters, [FromQuery] Guid storeId)
+        {
+            var pagedResult = await _adminMenuItemService.GetAllMenuItemAsync(urlQueryParameters, storeId);
+            return Ok(pagedResult);
+        }
+
+        [HttpGet("menu-item/{menuItemId}")]
+        public async Task<IActionResult> GetMenuItemDetail(Guid menuItemId)
+        {
+            var menuItems = await _adminMenuItemService.GetMenuItemDetail(menuItemId, StoreId);
+            if (menuItems == null)
+                return NotFound();
+
+            return Ok(menuItems);
+        }
+
+        [HttpPut("menu-item/{id}")]
+        public async Task<IActionResult> UpdateMenuItem(Guid id, [FromBody] MenuItemAdminServiceDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var updated = await _adminMenuItemService.UpdateMenuAsync(id, dto, UserId);
+            if (!updated)
+                return NotFound();
+
+            return NoContent(); // 204
+        }
+
+        [HttpDelete("menu-item/{id}")]
+        public async Task<IActionResult> DeleteMenuItem(Guid id)
+        {
+            var deleted = await _adminMenuItemService.DeleteMenuAsync(id, UserId);
+            if (!deleted)
+                return NotFound();
+
+            return NoContent(); // 204
+        }
+
+    }
+}
---
+++ SEP490-FOCS/FOCS/Controllers/AdminStoreController.cs
@@ -0,0 +1,53 @@
+﻿using FOCS.Application.DTOs.AdminServiceDTO;
+using FOCS.Application.Services.Interface;
+using FOCS.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FOCS.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminStoreController : FocsController
+    {
+        private readonly IStoreManagementService _adminStoreService;
+
+        public AdminStoreController(IStoreManagementService storeService)
+        {
+            _adminStoreService = storeService;
+        }
+
+        [HttpPost("store")]
+        public async Task<IActionResult> CreateStore([FromBody] StoreAdminServiceDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _adminStoreService.CreateStoreAsync(dto, UserId);
+            return Ok(created);
+        }
+
+        [HttpPost("stores")]
+        public async Task<IActionResult> GetStores([FromBody] UrlQueryParameters query)
+        {
+            var result = await _adminStoreService.GetAllStoresAsync(query);
+            return Ok(result);
+        }
+
+        [HttpPut("store/{id}")]
+        public async Task<IActionResult> UpdateStore(Guid id, [FromBody] StoreAdminServiceDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var success = await _adminStoreService.UpdateStoreAsync(id, dto, UserId);
+            return success ? Ok() : NotFound();
+        }
+
+        [HttpDelete("store/{id}")]
+        public async Task<IActionResult> DeleteStore(Guid id)
+        {
+            var success = await _adminStoreService.DeleteStoreAsync(id, UserId);
+            return success ? Ok() : NotFound();
+        }
+    }
+}
