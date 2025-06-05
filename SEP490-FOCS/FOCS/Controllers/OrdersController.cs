7f8ee0e5c4596ddd6706e33ecef6193733b5f27c
xuansonhoang-tech <156825110+xuansonhoang-tech@users.noreply.github.com>
2025-06-05 10:51:12 +0700

    hot fix recode main (#77)
    Co-authored-by: XuanSonHoang <hson512475@gmail.com>

---
+++ SEP490-FOCS/FOCS/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using FOCS.Application.DTOs;
+using FOCS.Common.Constants;
+using FOCS.Realtime.Hubs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FOCS.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class OrdersController : FocsController
+    {
+        //SignalR 
+        private readonly IHubContext<OrderHub> _orderHubContext;
+
+        public OrdersController(IHubContext<OrderHub> hubContext)
+        {
+            _orderHubContext = hubContext;  
+        }
+
+        [HttpPost("notify-kitchen")]
+        public async Task NotifyKitchenAsync(OrderWrapDTO orderWrapDTO)
+        {
+            var group = SignalRGroups.Kitchen(orderWrapDTO.StoreId);
+            await _orderHubContext.Clients.Group(group).SendAsync(Constants.Method.ReceiveOrderWrapUpdate, orderWrapDTO);
+        }
+
+    }
+}
