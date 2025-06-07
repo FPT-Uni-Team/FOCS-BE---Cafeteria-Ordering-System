2af8732150b4a4ba87d8bfdc1de0d026ab1b5bba
phucnh <phucnhhe172629@fpt.edu.vn>
2025-06-07 21:00:32 +0700

    perf: check store setting exist

--- SEP490-FOCS/FOCS.Application/DTOs/AdminServiceDTO/StoreSettingDTO.cs
+++ SEP490-FOCS/FOCS.Application/DTOs/AdminServiceDTO/StoreSettingDTO.cs
@@ -1,24 +1,33 @@
 ﻿using FOCS.Common.Enums;
 using FOCS.Order.Infrastucture.Entities;
+using System.Text.Json.Serialization;
 
 namespace FOCS.Common.Models
 {
     public class StoreSettingDTO
     {
+        [JsonPropertyName("id")]
         public Guid? Id { get; set; }
 
+        [JsonPropertyName("open_time")]
         public TimeSpan? OpenTime { get; set; }
 
+        [JsonPropertyName("close_time")]
         public TimeSpan? CloseTime { get; set; }
 
+        [JsonPropertyName("currency")]
         public string? Currency {  get; set; } = "VND";
 
+        [JsonPropertyName("payment_config")]
         public PaymentConfig? PaymentConfig { get; set; }
 
-        public string? logoUrl { get; set; } = "";
+        [JsonPropertyName("logo_url")]
+        public string? LogoUrl { get; set; } = "";
 
+        [JsonPropertyName("is_self_service")]
         public bool? IsSelfService { get; set; }
 
-        public DiscountStrategy? discountStrategy { get; set; }
+        [JsonPropertyName("discount_strategy")]
+        public DiscountStrategy? DiscountStrategy { get; set; }
     }
 }
