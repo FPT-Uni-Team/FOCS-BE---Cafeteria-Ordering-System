0d597ddcbaff9870e8bade8f9122a62ece7f4f7c
phucnhhe172629 <155723231+phucnhhe172629@users.noreply.github.com>
2025-06-09 22:51:48 +0700

    ref: update AcceptForItems to List<Guid> (#95)

---
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Migrations/20250609065232_update_item_link_promotion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FOCS.Order.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_item_link_promotion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
