eb7e133aea99d821751964c2bfcb64d97948d151
xuansonhoang-tech <156825110+xuansonhoang-tech@users.noreply.github.com>
2025-06-13 18:14:58 +0700

    fix create coupon and list coupon (#109)
    Co-authored-by: XuanSonHoang <hson512475@gmail.com>

---
+++ SEP490-FOCS/FOCS.Common/Models/SetCouponConditionRequest.cs
@@ -0,0 +1,19 @@
+﻿using FOCS.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FOCS.Common.Models
+{
+    public class SetCouponConditionRequest
+    {
+        [JsonPropertyName("condition_type")]
+        public CouponConditionType ConditionType { get; set; }
+
+        [JsonPropertyName("value")]
+        public int Value { get; set; }
+    }
+}
