71d873a188cd3511d61ed945df3c493a6143ef85
phucnhhe172629 <155723231+phucnhhe172629@users.noreply.github.com>
2025-06-15 14:30:45 +0700

    feat: user profile (#111)

---
+++ SEP490-FOCS/FOCS.Application/Services/Interface/IUserProfileService.cs
@@ -0,0 +1,13 @@
+﻿using FOCS.Application.DTOs;
+using FOCS.Application.DTOs.AdminServiceDTO;
+using FOCS.Common.Models;
+
+namespace FOCS.Application.Services.Interface
+{
+    public interface IUserProfileService
+    {
+        Task<UserProfileDTO> GetUserProfileAsync(string userId);
+        Task<UserProfileDTO> UpdateUserProfileAsync(UserProfileDTO dto, string userId);
+        Task<bool> DeleteUserProfileAsync(string userId);
+    }
+}
