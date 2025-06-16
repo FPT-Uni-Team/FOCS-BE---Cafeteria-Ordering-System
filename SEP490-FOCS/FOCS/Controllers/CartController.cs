34ba3a61d43f74df6652e44c59dc725d7e2a0fb5
xuansonhoang-tech <156825110+xuansonhoang-tech@users.noreply.github.com>
2025-06-16 22:15:55 +0700

    update code cart (#119)
    * update code checkout flow: apply coupon and promotion
    
    * update code checkout: apply promotion and coupon, implement get order detail
    
    * update code add cart for actor
    
    ---------
    
    Co-authored-by: XuanSonHoang <hson512475@gmail.com>
    Co-authored-by: Hoàng Xuân Sơn <114411282+xn-son-tech@users.noreply.github.com>

--- SEP490-FOCS/FOCS/Controllers/CartController.cs
+++ SEP490-FOCS/FOCS/Controllers/CartController.cs
@@ -1,11 +1,12 @@
 ﻿using FOCS.Common.Interfaces;
+using FOCS.Common.Models;
 using FOCS.Common.Models.CartModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FOCS.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("order")]
     [ApiController]
     public class CartController : FocsController
     {
@@ -16,11 +17,30 @@ namespace FOCS.Controllers
             _cartService = cartService;
         }
 
-        [HttpPost("tables/{tableId}/cart/{actorId}")]
-        public async Task<IActionResult> AddOrUpdateCartAsync(Guid tableId, string actorId, [FromBody] CartItemRedisModel model)
+        [HttpPost("tables/{tableId}/cart/{actorId?}")]
+        public async Task<IActionResult> AddOrUpdateCartAsync(Guid tableId, string? actorId, [FromBody] CartItemRedisModel model)
         {
             await _cartService.AddOrUpdateItemAsync(tableId, actorId ?? UserId, model, StoreId);
             return Ok();
         }
+
+        [HttpPost("clear/table/{tableId}/cart/{actorId}")]
+        public async Task ClearCartAsync(Guid tableId, string actorId)
+        {
+            await _cartService.ClearCartAsync(tableId, StoreId, actorId);
+        }
+
+        [HttpPost("get/table/{tableId}/cart/{actorId}")]
+        public async Task<List<CartItemRedisModel>> GetCartAsync(Guid tableId, string actorId)
+        {
+            return await _cartService.GetCartAsync(tableId, StoreId, actorId);
+        }
+
+        [HttpDelete("table/{tableId}/cart/{actorId}")]
+        public async Task RemoveItemCartAsync(Guid tableId, string actorId, [FromBody] RemoveItemCartRequest request)
+        {
+            await _cartService.RemoveItemAsync(tableId, actorId, StoreId, request.MenuItemId, request.VariantId);
+        }
+
     }
 }
