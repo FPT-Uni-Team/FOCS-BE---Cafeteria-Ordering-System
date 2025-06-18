f8335e5823674857f5379593d870cd23a75ae911
phucnh <phucnhhe172629@fpt.edu.vn>
2025-06-18 16:10:25 +0700

    feat: assign, remove role staff

---
+++ SEP490-FOCS/FOCS.Common/Models/UpdateRoleRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FOCS.Common.Models
+{
+    public class UpdateRoleRequest
+    {
+        [Required]
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [Required]
+        [JsonPropertyName("user_to_update_id")]
+        public string UserToUpdateId { get; set; }
+    }
+
+}
