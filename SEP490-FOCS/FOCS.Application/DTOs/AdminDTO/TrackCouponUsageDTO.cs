ccd146cd9b083b0c3e8c4c69e3040a2d366b9104
Thai Duc <dioak2002@gmail.com>
2025-06-22 23:24:52 +0700

    update/update coupon usage

---
+++ SEP490-FOCS/FOCS.Application/DTOs/AdminDTO/TrackCouponUsageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FOCS.Application.DTOs.AdminDTO
+{
+    public class TrackCouponUsageDTO
+    {
+        public int TotalLeft { get; set; }
+        public int? LeftPerUser { get; set; }
+    }
+}
