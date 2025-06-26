b278681ba3fb99e6e62923a6fd6648958d95dfb7
xuansonhoang-tech <156825110+xuansonhoang-tech@users.noreply.github.com>
2025-06-26 19:40:17 +0700

    update get variant group and variant by menu item (#148)
    Co-authored-by: XuanSonHoang <hson512475@gmail.com>

--- SEP490-FOCS/FOCS.Application/Services/MenuItemsVariantGroupService.cs
+++ SEP490-FOCS/FOCS.Application/Services/MenuItemsVariantGroupService.cs
@@ -1,15 +1,21 @@
 ﻿using AutoMapper;
 using FOCS.Application.DTOs.AdminServiceDTO;
 using FOCS.Application.Services.Interface;
+using FOCS.Common.Exceptions;
 using FOCS.Common.Interfaces;
 using FOCS.Common.Models;
+using FOCS.Common.Utils;
 using FOCS.Infrastructure.Identity.Common.Repositories;
 using FOCS.Order.Infrastucture.Entities;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Esf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using static FOCS.Common.Exceptions.Errors;
 
 namespace FOCS.Application.Services
 {
@@ -49,6 +55,40 @@ namespace FOCS.Application.Services
             {
                 return new List<MenuItemVariantGroup>();
             }
+        }
+
+        public async Task<GetVariantGroupAndVariantResponse> GetVariantGroupsWithVariants(Guid menuItemId, Guid storeId)
+        {
+            var variantGroups = await _menuItemVariantGroupRepository.AsQueryable().Include(x => x.VariantGroup).
+                                                                                    ThenInclude(x => x.Variants.Where(x => x.CreatedBy == storeId.ToString())).
+                                                                                    Where(x => x.MenuItemId == menuItemId).ToListAsync();
+
+            ConditionCheck.CheckCondition(variantGroups != null, Errors.Common.NotFound);
+
+            var rs = new GetVariantGroupAndVariantResponse
+            {
+                Group = new Dictionary<string, List<GetVariantGroupResponse>>()
+            };
+
+            foreach (var groupWrapper in variantGroups)
+            {
+                var groupName = groupWrapper.VariantGroup.Name;
+
+                if (!rs.Group.ContainsKey(groupName))
+                {
+                    rs.Group[groupName] = new List<GetVariantGroupResponse>();
+                }
+
+                foreach (var variant in groupWrapper.VariantGroup.Variants)
+                {
+                    if (!rs.Group[groupName].Any(v => v.Name == variant.Name))
+                    {
+                        rs.Group[groupName].Add(new GetVariantGroupResponse { Name = variant.Name });
+                    }
+                }
+            }
+
+            return rs;
         }
     }
 }
