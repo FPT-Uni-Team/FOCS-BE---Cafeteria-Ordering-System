e57d50986b37d6fc511dac8af4a23e16bb01a432
Njlou <137082443+Njlou@users.noreply.github.com>
2025-07-14 20:44:04 +0700

    update/cors for fe (#202)

--- SEP490-FOCS/FOCS/Program.cs
+++ SEP490-FOCS/FOCS/Program.cs
@@ -218,7 +218,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500") 
+        policy.WithOrigins("http://127.0.0.1:5500",
+                           "https://focs-admin-site-git-main-cuongncs-projects.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); 
