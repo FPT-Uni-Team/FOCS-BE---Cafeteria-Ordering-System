bdd4d9e79662b0bd27bb07bc63dfeaf472511bac
Hoàng Xuân Sơn <114411282+xn-son-tech@users.noreply.github.com>
2025-07-21 01:21:01 +0700

    hot fix (#238)
    * update code for create table
    
    * update qr code add
    
    * hot fix

--- SEP490-FOCS/FOCS.Application/DTOs/TableDTO.cs
+++ SEP490-FOCS/FOCS.Application/DTOs/TableDTO.cs
@@ -14,6 +14,9 @@ namespace FOCS.Application.DTOs
         [JsonPropertyName("status")]
         public TableStatus Status { get; set; }
 
+        [JsonPropertyName("qr_code")]
+        public string? QrCode { get; set; }
+        
         [JsonPropertyName("is_active")]
         public bool IsActive { get; set; }
         
