e13f57539d2ae51f802649e3868bdc52e5514161
XuanSonHoang <hson512475@gmail.com>
2025-08-11 00:30:14 +0700

    update code for otp

---
+++ SEP490-FOCS/FOCS.Order.Infrastucture/Migrations/20250810151016_update_new_version.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FOCS.Order.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_new_version : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "OrderWraps",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "OrderWraps",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "OrderWraps",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "OrderWraps",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "OrderWraps");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "OrderWraps");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "OrderWraps");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "OrderWraps");
+        }
+    }
+}
