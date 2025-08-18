b0ff11c2978694ad6ad22d2f5d9967cc8eb06b89
xn-son-tech <sonhxhe172036@fpt.edu.vn>
2025-08-19 00:45:07 +0700

    update code checkout

---
+++ SEP490-FOCS/FOCS.Common/Models/CouponUsageResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FOCS.Common.Models
+{
+    public class CouponUsageResponse
+    {
+        [JsonPropertyName("coupon_code")]
+        public string CouponCode { get; set; }
+
+        [JsonPropertyName("order_code")]
+        public string OrderCode { get; set; }
+
+        [JsonPropertyName("actor_id")]
+        public string ActorId { get; set; }
+
+        [JsonPropertyName("used_at")]
+        public DateTime UsedAt { get; set; }
+    }
+}
