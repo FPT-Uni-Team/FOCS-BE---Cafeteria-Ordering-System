b92f57be66af46f78a5346b95a34f886ec5252a9
xn-son-tech <sonhxhe172036@fpt.edu.vn>
2025-08-23 19:58:38 +0700

    update code for statistics

---
+++ SEP490-FOCS/FOCS.Common/Models/Dashboard/OrderReportDayResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FOCS.Common.Models.Dashboard
+{
+    public class OrderReportDayResponse
+    {
+        [JsonPropertyName("pending_orders")]
+        public int PendingOrders { get; set; }
+
+        [JsonPropertyName("inprogress_orders")]
+        public int InProgressOrders { get; set; }
+
+        [JsonPropertyName("completed_orders")]
+        public int CompletedOrders { get; set; }
+
+        [JsonPropertyName("cancelled_orders")]
+        public int CanceledOrders { get; set; }
+
+        [JsonPropertyName("average_complete_time")]
+        public double AvetageCompleteTime { get; set; }
+    }
+}
---
+++ SEP490-FOCS/FOCS/Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using FOCS.Application.Services.Interface;
+using FOCS.Common.Interfaces;
+using FOCS.Common.Models;
+using FOCS.Common.Models.Dashboard;
+using FOCS.Order.Infrastucture.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FOCS.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class DashboardController : FocsController
+    {
+       private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpPost("order-statistic")]
+        public async Task<OrderReportDayResponse> OrderReportDayResponse([FromHeader] bool? today = null)
+        {
+            return await _dashboardService.GetOrderStatsAsync(StoreId, today);
+        }
+
+        [HttpPost("overview")]
+        public async Task<OverviewDayResponse> GetOverviewAsync([FromHeader] bool? today = null)
+        {
+            return await _dashboardService.GetOverviewAsync(StoreId, today);
+        }
+
+        [HttpPost("kitchen-statistic")]
+        public async Task<ProdOrderReportResponse> GetKitchenStatsAsync([FromHeader] bool? today = null)
+        {
+            return await _dashboardService.GetKitchenStatsAsync(StoreId, today);
+        }
+
+        [HttpPost("finance-statistic")]
+        public async Task<RevenueReportResponse> GetFinanceStatsAsync()
+        {
+            return await _dashboardService.GetFinanceStatsAsync(StoreId);
+        }
+    }
+}
