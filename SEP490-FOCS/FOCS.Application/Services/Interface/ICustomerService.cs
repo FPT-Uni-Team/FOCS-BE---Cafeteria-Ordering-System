02741ead19ef41ce647626358ccb0fdb94efb6a1
phucnh <phucnhhe172629@fpt.edu.vn>
2025-08-28 00:55:42 +0700

    feat: customer

---
+++ SEP490-FOCS/FOCS.Application/Services/Interface/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using FOCS.Application.DTOs;
+using FOCS.Application.DTOs.AdminServiceDTO;
+using FOCS.Common.Constants;
+using FOCS.Common.Models;
+
+namespace FOCS.Application.Services.Interface
+{
+    public interface ICustomerService
+    {
+        Task<PagedResult<UserProfileDTO>> GetCustomerListAsync(UrlQueryParameters query, string storeId);
+        Task<UserProfileDTO> GetCustomerProfileAsync(string storeId, string customerId, string managerId);
+        Task<bool> BlockCustomerAsync(string storeId, string customerId, string managerId);
+        Task<bool> UnblockCustomerAsync(string storeId, string customerId, string managerId);
+    }
+}
---
+++ SEP490-FOCS/FOCS/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using FOCS.Application.DTOs;
+using FOCS.Application.DTOs.AdminServiceDTO;
+using FOCS.Application.Services.Interface;
+using FOCS.Common.Constants;
+using FOCS.Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FOCS.Controllers
+{
+    [Route("api/admin/customer")]
+    [ApiController]
+    public class CustomerController : FocsController
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public async Task<PagedResult<UserProfileDTO>> GetCustomerList([FromBody] UrlQueryParameters query)
+        {
+            return await _customerService.GetCustomerListAsync(query, StoreId);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<UserProfileDTO> GetBrandDetail(string customerId)
+        {
+            return await _customerService.GetCustomerProfileAsync(StoreId, customerId, UserId);
+        }
+
+        [HttpPatch("{customerId}/block")]
+        public async Task<bool> BlockCustomer(string customerId)
+        {
+            return await _customerService.BlockCustomerAsync(StoreId, customerId, UserId);
+        }
+
+        [HttpPatch("{customerId}/block")]
+        public async Task<bool> UnblockCustomer(string customerId)
+        {
+            return await _customerService.UnblockCustomerAsync(StoreId, customerId, UserId);
+        }
+
+    }
+}
